38818237b8de226a6c44fe50eb30f60c09ee5eca
Eavilesz <ernesto-av@hotmail.com>
2022-01-25 21:28:30 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ListaQuehaceres from "./components/ListaQuehaceres";
+
+function App() {
+  return (
+    <div className="App">
+      <ListaQuehaceres />
+    </div>
+  );
+}
+
+export default App;
